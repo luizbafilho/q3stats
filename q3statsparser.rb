d00067ce70e4047848b993047fd614122dd1e285
Luiz Filho <luizbafilho@gmail.com>
2012-12-26 00:49:22 -0300

    Finishing up the parser

--- q3statsparser.rb
+++ q3statsparser.rb
@@ -1,11 +1,45 @@
+require './string.rb'
 require './game.rb'
 require './player.rb'
+require 'jbuilder'
 
 class Q3StatsParser
 
   def initialize
     @log_lines = File.readlines('logs/games.log')
     @games = []
+  end
+
+  def parse
+    @log_lines.each do |line|
+      if line.game?
+        add_game
+      elsif line.player?
+        current_game.add_player(Player.new(line.raw_player))
+      elsif line.kill?
+        current_game.add_kill(line.raw_kill)
+      end
+    end
+  end
+
+  def add_game
+    @games << Game.new
+  end
+
+  def to_builder
+    json = Jbuilder.new
+    json.array!(@games) do |game|
+      json.total_kills game.total_kills
+      json.players game.players.map {|p| p.name}
+      json.kills game.players, :name, :kills
+    end
+    json
+  end
+
+  private
+
+  def current_game
+    @games.last
   end
 
 end
