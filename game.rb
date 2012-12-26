d684d866e3a7943fde4235ef88d2d598662db9a3
Luiz Filho <luizbafilho@gmail.com>
2012-12-26 00:08:54 -0300

    Defining game features

--- game.rb
+++ game.rb
@@ -7,4 +7,43 @@ class Game
     @players = []
   end
 
+  def add_player player
+    p = player_by_id(player.id)
+    if !p
+      @players << player
+    elsif p.name != player.name
+      p.name = player.name
+    end
+  end
+
+  def add_kill raw_kill
+    @total_kills += 1
+    p = player_by_name raw_kill.first
+    if p.present?
+      p.add_kill
+    else
+      killed_player = player_by_name raw_kill.second
+      killed_player.remove_kill
+    end
+  end
+
+  def kills
+    kills = {}
+
+    @players.each do |player|
+      kills[player.name] = player.kills
+    end
+    kills
+  end
+
+  private
+
+  def player_by_id id
+    @players.select {|p| p.id == id}.first
+  end
+
+  def player_by_name name
+    @players.select {|p| p.name == name}.first
+  end
+
 end
