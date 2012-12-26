4bae45e41914fab78000873e90a3302589500a5c
Luiz Filho <luizbafilho@gmail.com>
2012-12-26 18:02:36 -0300

    Player specs

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -19,4 +19,28 @@ describe Player do
       @player.kills.should eq(0)
     end
   end
+
+  describe "#add_kill" do
+
+    before do
+      @player = Player.new([0, "Test"])
+    end
+
+    it "adds one kill to the player" do
+      @player.add_kill
+      @player.kills.should eq(1)
+    end
+  end
+
+  describe "#removekill" do
+
+    before do
+      @player = Player.new([0, "Test"])
+    end
+
+    it "removes one kill from the player" do
+      @player.remove_kill
+      @player.kills.should eq(-1)
+    end
+  end
 end
