58bb14929d8535851f4b5916c4890b528fbcdb16
Luiz Filho <luizbafilho@gmail.com>
2012-12-26 23:24:05 -0300

    Docs for game and event

--- event.rb
+++ event.rb
@@ -1,6 +1,24 @@
-class Event
 
-  attr_reader :type, :captures
+# Public: Represents a event occured on the games.log.
+# For each relevant line that represents some event, like a game initiating,
+# a new player or a player killing other player, tt is created on Event Object, to
+# represent this line on the application.
+#
+class Event
+  # Public: Represents the type of the event
+  # that can be: :game, :player or :kill
+  attr_reader :type
+
+  # Public: Represents relevant content grabbed by the regex in the games.log file like the the player name.
+  #
+  # Examples
+  #
+  #   # For the Player event
+  #   # => [1, 'Player 1']
+  #
+  #   # For the  Kill event
+  #   # => ['Player 1', 'Player 2', 'MOD_ROCKET']
+  attr_reader :captures
 
   def initialize type, captures
     @type = type
--- game.rb
+++ game.rb
@@ -1,12 +1,17 @@
 class Game
 
+  # Public: Gets/Sets total_kills and players.
   attr_accessor :total_kills, :players
 
+  # Public: Initialize a Game.
   def initialize
     @total_kills = 0
     @players = []
   end
 
+  # Public: Adds the user to the players array
+  #
+  # player -Player object
   def add_player player
     p = player_by_id(player.id)
     if !p
@@ -16,6 +21,15 @@ class Game
     end
   end
 
+  # Public: Adds a kill to the user. If the killer is the <world> then the user
+  # has one kill removed
+  #
+  # raw_kill -Raw data returned by the regex match
+  #
+  # Examples
+  #
+  #    add_kill(['Player 1', 'Player 2', 'MOD_ROCKET'])
+  #
   def add_kill raw_kill
     @total_kills += 1
     p = player_by_name raw_kill.first
@@ -29,10 +43,20 @@ class Game
 
   private
 
+  # Private: Finds a user by ID in the players array.
+  #
+  # id -Player ID
+  #
+  # Returns the found user.
   def player_by_id id
     @players.select {|p| p.id == id}.first
   end
 
+  # Private: Finds a user by name in the players array.
+  #
+  # name - Player name
+  #
+  # Returns the found user.
   def player_by_name name
     @players.select {|p| p.name == name}.first
   end
