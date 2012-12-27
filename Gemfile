93b0ccd69a9162e68c433abacce00976f271f355
Luiz Filho <luizbafilho@gmail.com>
2012-12-26 22:47:54 -0300

    Adding basic documentation

--- Gemfile
+++ Gemfile
@@ -1,8 +1,13 @@
 # A sample Gemfile
+source :rubygems
 source "https://rubygems.org"
-
+source :gemcutter
+source "http://gemcutter.org"
 # gem "rails"
 
+gem 'yard'
+gem 'yard-tomdoc'
+gem 'tomdoc'
 gem 'jbuilder'
 gem 'sinatra'
 gem 'rspec'
--- player.rb
+++ player.rb
@@ -1,17 +1,24 @@
 class Player
+  # Public: Returns the id of the user, it is a ID grabbed from the log file.
   attr_reader :id
+  # Public: Gets/Sets for the users's name and the users kills.
   attr_accessor :name, :kills
 
+  # Public: Initialize a Player.
+  #
+  # raw_player - A array which the first element is the ID and the sacond one is the name.
   def initialize raw_player
     @id = raw_player[0]
     @name = raw_player[1]
     @kills = 0
   end
 
+  # Public:  Increase the user kills counter.
   def add_kill
     @kills += 1
   end
 
+  # Public:  Decrease the user kills counter.
   def remove_kill
     @kills -= 1
   end
