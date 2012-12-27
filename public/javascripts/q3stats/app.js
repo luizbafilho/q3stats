9bdb1a60daabd869a0b0e8be0f28b8c036202c55
Luiz Filho <luizbafilho@gmail.com>
2012-12-27 13:54:44 -0300

    Adding javascript for tables loading

---
+++ public/javascripts/q3stats/app.js
@@ -0,0 +1,20 @@
+if(typeof Q3Stats === "undefined") {
+  var Q3Stats = {};
+}
+  
+Q3Stats.App = function(){
+
+  var _init = function(games){
+    this.games = games
+  }
+
+  var _setCurrentGame = function(index){
+    this.currentGame = this.games[index];
+  }
+
+  return {
+    init: _init,
+    setCurrentGame: _setCurrentGame
+  }
+
+}();
