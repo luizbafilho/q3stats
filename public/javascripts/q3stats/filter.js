4ca60a3ec2a19183b83e1c5e9d0e1b4caccf9310
Luiz Filho <luizbafilho@gmail.com>
2012-12-27 14:35:28 -0300

    Adding filter

---
+++ public/javascripts/q3stats/filter.js
@@ -0,0 +1,29 @@
+if(typeof Q3Stats === "undefined") {
+  var Q3Stats = {};
+}
+
+Q3Stats.Filter = function(){
+  var _setupChange = function(){
+   $('#filter').change(function(){
+        if(this.value == -1){
+            Q3Stats.Tables.renderAll();
+        }else{
+            Q3Stats.Tables.render(this.value);
+        }
+    });
+  }
+
+  var _loadGames = function(){
+    var self = this;
+    self.$filter = $('#filter');
+    $(Q3Stats.App.games).each(function(index, game){
+      $('<option>').text('Game ' + index).attr('value', index).appendTo(self.$filter)
+    });
+  } 
+  
+  return{
+    loadGames: _loadGames,
+    setupChange: _setupChange
+  } 
+
+}();
